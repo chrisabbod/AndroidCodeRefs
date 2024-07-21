17046044575172a9976883d544c2f0f01634b833
chris_abbod <chrisabbod@gmail.com>
2024-07-21 17:56:43 -0400

    App able to build and run

--- cupcake/src/main/java/com/chrisabbod/cupcake/CupcakeScreen.kt
+++ cupcake/src/main/java/com/chrisabbod/cupcake/CupcakeScreen.kt
@@ -1,20 +1,39 @@
 package com.chrisabbod.cupcake
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.chrisabbod.cupcake.ui.theme.CupcakeTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.chrisabbod.cupcake.data.DataSource
+
+enum class CupcakeScreen() {
+    Start,
+    Flavor,
+    Pickup,
+    Summary
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -43,12 +62,35 @@ fun CupcakeAppBar(
 }
 
 @Composable
-fun CupcakeApp() {
-    // TODO: Add 2 parameters and scaffold
-    CupcakeAppBar(
-        canNavigateBack = false,
-        navigateUp = { /*TODO: Implement back navigation*/ }
-    )
+fun CupcakeApp(
+    viewModel: OrderViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+
+    Scaffold(
+        topBar = {
+            CupcakeAppBar(
+                canNavigateBack = false,
+                navigateUp = { /*TODO*/ }
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = CupcakeScreen.Start.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = CupcakeScreen.Start.name) {
+                StartOrderScreen(
+                    quantityOptions = DataSource.quantityOptions,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(id = R.dimen.padding_medium))
+                )
+            }
+        }
+    }
 }
 
 @Preview(showSystemUi = true, showBackground = true)
---
+++ cupcake/src/main/java/com/chrisabbod/cupcake/OrderViewModel.kt
@@ -0,0 +1,41 @@
+package com.chrisabbod.cupcake
+
+import android.icu.util.Calendar
+import androidx.lifecycle.ViewModel
+import com.chrisabbod.cupcake.data.OrderUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
+
+/**
+ * [OrderViewModel] holds information about a cupcake order in terms of quantity, flavor, and
+ * pickup date. It also knows how to calculate the total price based on these order details.
+ */
+class OrderViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(OrderUiState(pickupOptions = pickupOptions()))
+    val uiState: StateFlow<OrderUiState> = _uiState.asStateFlow()
+
+
+
+    /**
+     * Returns a list of date options starting with the current date and the following 3 dates.
+     */
+    private fun pickupOptions(): List<String> {
+        val dateOptions = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        // Add current date and following 3 dates
+        repeat(4) {
+            dateOptions.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return dateOptions
+    }
+
+}
