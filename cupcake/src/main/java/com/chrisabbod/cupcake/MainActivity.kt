b790d58ce7796ec44dc300014de1722395f6446e
chris_abbod <chrisabbod@gmail.com>
2024-07-21 15:28:46 -0400

    Add CupcakeTheme

--- cupcake/src/main/java/com/chrisabbod/cupcake/MainActivity.kt
+++ cupcake/src/main/java/com/chrisabbod/cupcake/MainActivity.kt
@@ -3,13 +3,13 @@ package com.chrisabbod.cupcake
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.chrisabbod.cupcake.ui.theme.AndroidCodeRefsTheme
+import com.chrisabbod.cupcake.ui.theme.CupcakeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidCodeRefsTheme {
+            CupcakeTheme {
                CupcakeApp()
             }
         }
