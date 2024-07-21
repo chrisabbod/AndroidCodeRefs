29d69b01e5088d337ab92a430fff96522e0065e8
chris_abbod <chrisabbod@gmail.com>
2024-07-21 15:40:12 -0400

    Create CupcakeAppBar compose function

--- cupcake/src/main/java/com/chrisabbod/cupcake/CupcakeScreen.kt
+++ cupcake/src/main/java/com/chrisabbod/cupcake/CupcakeScreen.kt
@@ -1,9 +1,60 @@
 package com.chrisabbod.cupcake
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.chrisabbod.cupcake.ui.theme.CupcakeTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CupcakeAppBar(
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(id = R.string.app_name))},
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
 
 @Composable
 fun CupcakeApp() {
-    Text(text = "HELLO CUPCAKE")
+    // TODO: Add 2 parameters and scaffold
+    CupcakeAppBar(
+        canNavigateBack = false,
+        navigateUp = { /*TODO: Implement back navigation*/ }
+    )
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun CupcakeScreenPreview() {
+    CupcakeTheme {
+        CupcakeApp()
+    }
 }
