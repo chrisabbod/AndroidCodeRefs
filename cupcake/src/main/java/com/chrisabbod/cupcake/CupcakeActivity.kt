c7714b70912402e7370dfa15ce2af7095d98ca85
chris_abbod <chrisabbod@gmail.com>
2024-07-23 19:42:53 -0400

    Update Cupcake module MainActivity to CupcakeActivity

--- cupcake/src/main/java/com/chrisabbod/cupcake/CupcakeActivity.kt
+++ cupcake/src/main/java/com/chrisabbod/cupcake/CupcakeActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.chrisabbod.cupcake.ui.theme.CupcakeTheme
 
-class MainActivity : ComponentActivity() {
+class CupcakeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
