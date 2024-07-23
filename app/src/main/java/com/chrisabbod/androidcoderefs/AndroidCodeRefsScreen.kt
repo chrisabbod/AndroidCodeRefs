a0d666d877ac3ca58ce01f004fe7bc04c911c6e3
chris_abbod <chrisabbod@gmail.com>
2024-07-23 19:28:53 -0400

    Replaced MainList Column with LazyColumn and app can be put in Landscape again

--- app/src/main/java/com/chrisabbod/androidcoderefs/AndroidCodeRefsScreen.kt
+++ app/src/main/java/com/chrisabbod/androidcoderefs/AndroidCodeRefsScreen.kt
@@ -1,24 +1,72 @@
 package com.chrisabbod.androidcoderefs
 
+import android.annotation.SuppressLint
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.chrisabbod.androidcoderefs.data.DataSource
 import com.chrisabbod.androidcoderefs.ui.components.ListItem
+import com.chrisabbod.androidcoderefs.ui.theme.AndroidCodeRefsTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AndroidCodeRefsScreen() {
-    Column(
+
+    Scaffold(
+        topBar = { AndroidCodeRefsAppBar() }, // TODO: Make better theme including color matching for system bar
+        content = { AndroidCodeRefsMainList() }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AndroidCodeRefsAppBar() {
+    Column {
+        TopAppBar(
+            title = { Text(stringResource(id = R.string.app_name)) },
+            colors = TopAppBarDefaults.mediumTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            )
+        )
+        HorizontalDivider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = Color.Black
+        )
+    }
+
+}
+
+@Composable
+fun AndroidCodeRefsMainList() {
+    LazyColumn(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        DataSource.moduleList.forEach { item ->
+        items(DataSource.moduleList) { item ->
             ListItem(item)
         }
     }
@@ -27,5 +75,7 @@ fun AndroidCodeRefsScreen() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun AndroidCodeRefsScreenPreview() {
-    AndroidCodeRefsScreen()
+    AndroidCodeRefsTheme {
+        AndroidCodeRefsScreen()
+    }
 }
--- app/src/main/java/com/chrisabbod/androidcoderefs/ui/components/ListItem.kt
+++ app/src/main/java/com/chrisabbod/androidcoderefs/ui/components/ListItem.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.chrisabbod.androidcoderefs.data.DataSource
 import com.chrisabbod.androidcoderefs.data.ModuleData
 
 @Composable
@@ -20,7 +24,12 @@ fun ListItem(item: ModuleData) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = item.name, style = MaterialTheme.typography.titleLarge)
@@ -28,4 +37,10 @@ fun ListItem(item: ModuleData) {
             Text(text = item.description, style = MaterialTheme.typography.bodySmall)
         }
     }
+}
+
+@Preview
+@Composable
+fun ListItemPreview() {
+    ListItem(DataSource.moduleList.get(0))
 }
