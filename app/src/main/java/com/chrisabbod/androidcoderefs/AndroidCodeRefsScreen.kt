56b400f9da3cab839bc92228de2b64c11c730e03
chris_abbod <chrisabbod@gmail.com>
2024-07-23 19:29:52 -0400

    Remove unused imports

--- app/src/main/java/com/chrisabbod/androidcoderefs/AndroidCodeRefsScreen.kt
+++ app/src/main/java/com/chrisabbod/androidcoderefs/AndroidCodeRefsScreen.kt
@@ -1,10 +1,8 @@
 package com.chrisabbod.androidcoderefs
 
 import android.annotation.SuppressLint
-import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
--- app/src/main/java/com/chrisabbod/androidcoderefs/MainActivity.kt
+++ app/src/main/java/com/chrisabbod/androidcoderefs/MainActivity.kt
@@ -3,13 +3,7 @@ package com.chrisabbod.androidcoderefs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.chrisabbod.androidcoderefs.ui.theme.AndroidCodeRefsTheme
 
